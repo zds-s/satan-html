a32015f5979c5d72d94b6e27513857627d6e7540
Death-satan <2703895209@qq.com>
2021-09-01 20:39:42 +0800

    提交代码

--- tests/HtmlManageTest.php
+++ tests/HtmlManageTest.php
@@ -9,7 +9,37 @@
 
 namespace SaTan\Html\Tests;
 
+use SaTan\Html\Exception\TagNotFoundException;
+use SaTan\Html\HtmlManage;
+use SaTan\Html\Tag;
+
 class HtmlManageTest extends \PHPUnit\Framework\TestCase
 {
+    protected HtmlManage $htmlManage;
+    protected function setUp(): void
+    {
+        $this->htmlManage = new HtmlManage();
+    }
+
+    public function testHtmlManage()
+    {
+        self::assertIsObject($this->htmlManage);
+    }
+
+    public function testTags()
+    {
+        self::assertIsArray($this->htmlManage->Tags());
+    }
+
+    public function testExistsTag()
+    {
+        self::assertIsBool($this->htmlManage->existsTag('test'));
+    }
+
+    public function testTag()
+    {
+        $this->expectException(TagNotFoundException::class);
+        $this->htmlManage->tag('test');
+    }
 
 }
